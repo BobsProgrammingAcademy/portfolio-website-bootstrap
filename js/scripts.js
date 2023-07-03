ebe77bc49c1cc082db79b18734472e3168fdc682
BobsProgrammingAcademy <bobsprogrammingacademy@gmail.com>
2023-07-03 15:05:50 +0200

    Code refactoring

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,6 @@ var map = L.map('map').setView([53.342686, -6.267118], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
-  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
