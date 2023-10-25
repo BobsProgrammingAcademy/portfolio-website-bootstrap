61738731c6d5c81ac4837bd2904c1812380628d5
BobsProgrammingAcademy <bobsprogrammingacademy@gmail.com>
2023-10-26 00:26:11 +0200

    Updated JS scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 // Leaflet Map
 
-var map = L.map('map').setView([53.342686, -6.267118], 13);
+const map = L.map('map').setView([53.342686, -6.267118], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
